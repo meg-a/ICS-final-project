dc5280195ff67e59063b907936b1e34dc319da7a
meg-a <80557260+meg-a@users.noreply.github.com>
2021-04-25 20:34:01 +0900

    Update meg_game.py

--- meg_game.py
+++ meg_game.py
@@ -6,7 +6,7 @@ import math
 wn = turtle.Screen()
 wn.title("game")
 wn.bgcolor("black")
-wn.setup(1000,1000)
+wn.setup(1200,800)
 wn.tracer(0)
 
 
@@ -19,7 +19,7 @@ pen = turtle.Turtle()
 pen.speed(0)
 pen.pensize(3)
 pen.shape("square")
-pen.color("white")
+pen.color("green")
 pen.penup()
 pen.hideturtle()
 
@@ -28,12 +28,13 @@ player.shape("square")
 player.color("white")
 player.speed(0)
 player.penup()
-player.width = 10
-player.height = 10
+player.width = 20
+player.height = 20
 player.dx = 0
 player.dy = 0
 player.state = "ready"
 player.goto(0, 0)
+
 
 
 GRAVITY = -0.3
@@ -45,17 +46,21 @@ class Block(object):
         self.y = y
         self.width = width
         self.height = height 
+        self.color = "green"
         
         
     def render(self, pen):
         pen.penup()
         pen.goto(self.x - self.width/2.0, self.y+self.height/2.0)
         pen.pendown()
+        pen.pencolor(self.color)
         pen.goto(self.x + self.width/2.0, self.y+self.height/2.0)
         pen.goto(self.x + self.width/2.0, self.y-self.height/2.0)
         pen.goto(self.x - self.width/2.0, self.y-self.height/2.0)
         pen.goto(self.x - self.width/2.0, self.y+self.height/2.0)
-        
+    
+    
+
         
 def jump():
     if player.state == "jumping_1":
@@ -69,8 +74,6 @@ def jump():
 def move_left():
     player.dx = -2
         
-    
-
 def move_right():
     player.dx = 2
     
@@ -82,10 +85,10 @@ def stop_down(ground,player_height):
     player.dy = 0
     player.state = "ready"
     
-def is_collision(sprite, block):
+def is_collision(player, block):
     # Axis Aligned Bounding Box
-    x_collision = (math.fabs(sprite.xcor() - block.x) * 2) < (sprite.width + block.width)
-    y_collision = (math.fabs(sprite.ycor() - block.y) * 2) < (sprite.height + block.height)
+    x_collision = (math.fabs(player.xcor() - block.x) * 2) < (player.width + block.width)
+    y_collision = math.fabs(player.ycor() - block.y) * 2 < (player.height + block.height)
     return (x_collision and y_collision)
 
 wn.listen()
@@ -96,16 +99,73 @@ wn.onkeyrelease(stop_left_right, "Left")
 wn.onkeyrelease(stop_left_right, "Right")
 
 blocks = []
-blocks.append(Block(0, -50, 100, 2))
-blocks.append(Block(-100, -100, 100, 2))
-blocks.append(Block(100, -100, 100, 2))
-blocks.append(Block(200, -150, 150, 2))
-blocks.append(Block(200, -120, 100, 2))
+blocks.append(Block(-600, 0, 2, 800))
+blocks.append(Block(0, -370, 1200,2))
+blocks.append(Block(-550, 300, 100, 2))
+blocks.append(Block(-500, 275, 2, 50))
+blocks.append(Block(-550, 250,100, 2))
+blocks.append(Block(-550, 175,150, 2))
+blocks.append(Block(-476, 136, 2, 75))
+blocks.append(Block(-380, 320, 2, 150))
+blocks.append(Block(-370, 245, 100, 2))
+blocks.append(Block(-190, 245, 100, 2))
+blocks.append(Block(-140, 320, 2, 150))
+blocks.append(Block(-252, 175,300, 2))
+blocks.append(Block(-425, 98,100, 2))
+blocks.append(Block(-170, 98,290, 2))
+blocks.append(Block(-376, 58,2, 77))
+blocks.append(Block(-25, 250, 2, 300))
+blocks.append(Block(-314, 23,2,150))
+blocks.append(Block(-400, 15,50,2))
+blocks.append(Block(-550, 15,100,2))
+blocks.append(Block(-402,-50,178,2))
+blocks.append(Block(-491,-100,2,100))
+blocks.append(Block(-420,-150,140,2))
+blocks.append(Block(-500,-250,250,2))
+blocks.append(Block(-170,-250,259,2))
+blocks.append(Block(-230,-140,2,470))
+blocks.append(Block(-140,-120,2,250))
+blocks.append(Block(-40,5,200,2))
+blocks.append(Block(60,-35,2,680))
+blocks.append(Block(160,305,200,2))
+blocks.append(Block(-255,-190,40,2))
+blocks.append(Block(36,-310,50,2))
+blocks.append(Block(36,-200,50,2))
+blocks.append(Block(-100,-170,80,2))
 
-for block in blocks:
-    block.render(pen)
+#entrance to teleport
+entrances = []
+entrances.append(Block(-595,-340, 3, 50))
+entrances.append(Block(-595,200, 3, 50))
+entrances.append(Block(-486,-120, 3, 50))
+entrances.append(Block(-135,-145,3,50))
+ 
+
+for entrance in entrances:
+    entrance.color = "red"
+
+#exit of the teleport
+exits = []
+exits.append(Block(-280,388,50,3))
+exits.append(Block(-150,-255,50,3))
+for exit in exits:
+    exit.color = "blue"
+
+gateways_list = [entrances,exits]
+
+player.goto(-500,400)
 
 while True:
+    pen.penup()
+    pen.goto(-430, 24)
+    pen.pendown()
+    pen.forward(120)
+    pen.right(1)
+    
+    
+  
+    
+       
     
     #Gravity
     player.dy += GRAVITY
@@ -119,35 +179,65 @@ while True:
     x += player.dx
     player.setx(x)
     
+    for gateways in gateways_list:
+        for gateway in gateways:
+            gateway.render(pen)
+            
+        
+        
+    
     #Deal with the ground
     for block in blocks:
+        block.render(pen)
         if is_collision(player, block):
-            player.sety(block.y+block.height/2.0 + player.height/2.0) 
-            player.dy = 0
-            player.state = "ready"
             
-    
-    
+             # Player is to the left
+            if player.xcor() < block.x - block.width/2.0 and player.dx > 0:
+                player.dx = 0
+                player.setx(block.x - block.width/2.0 - player.width/2.0)
+            # Player is to the right
+            elif player.xcor() > block.x + block.width/2.0 and player.dx < 0:
+                player.dx = 0
+                player.setx(block.x + block.width/2.0 + player.width/2.0)
+            # Player is above
+            elif player.ycor() > block.y:
+                player.dy = 0
+                player.sety(block.y + block.height/2.0 + player.height/2.0 - 1)
+            # Player is below
+            elif player.ycor() <= block.y:
+                player.dy = 0
+                player.sety(block.y - block.height/2.0 - player.height/2.0)
 
-    
+            player.state = "ready"
+                
+                
     wn.update()
-
-
-                       
+    pen.clear()
 
     
-def stay():
-    while True:
-        x = player.xcor()
-        player.setx(x+1)
+    print(str(player.xcor()) + " " + str(player.ycor()))
+    
+    
         
+    if player.xcor()<=-585 and ((player.ycor()>=180 and player.ycor()<=210)or(player.ycor()>=-360 and player.ycor() <=-330)):
+        player.goto(-280,400)
+        player.dy = 0
+        player.dx = 0
+                    
+    elif player.xcor() == -480  and (player.ycor()>=-144 and player.ycor()<=-95):
+        player.goto(-147,-255)
+        player.dy = 0
+        player.dx = 0
         
-
-
-
+    elif player.xcor() == -129  and (player.ycor()>=-162 and player.ycor()<=-120):
+        player.goto(-147,-255)
+        player.dy = 0
+        player.dx = 0
+        
 wn.onkeypress(up,"up")
 
 
 
 
 wn.mainloop()
+
