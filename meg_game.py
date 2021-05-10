fe71e078ac2ff8d22390859b2bfab0ce552f92e7
meg-a <80557260+meg-a@users.noreply.github.com>
2021-05-10 12:50:47 +0900

    Update meg_game.py

--- meg_game.py
+++ meg_game.py
@@ -1,16 +1,22 @@
 import turtle
 import math
+import os
+import time
 
 
 
 wn = turtle.Screen()
 wn.title("game")
-wn.bgcolor("black")
-wn.setup(1200,800)
+wn.bgcolor("sky blue")
+wn.setup(1220,800)
 wn.tracer(0)
 
+wn.bgpic('introduction.gif')
+wn.update()
+time.sleep(6)
+wn.bgpic("")
 
-ground_y = -100
+#ground_y = -100
 
 
 
@@ -18,14 +24,27 @@ ground_y = -100
 pen = turtle.Turtle()
 pen.speed(0)
 pen.pensize(3)
-pen.shape("square")
+pen.shape("circle")
 pen.color("green")
 pen.penup()
 pen.hideturtle()
 
+
+
+
+#life
+# ~ life = turtle.Turtle()
+# ~ life.speed(0)
+# ~ life.pensize(3)
+# ~ life.shape()
+# ~ life.color("white")
+# ~ life.penup()
+# ~ life.hideturtle()
+
+wn.addshape('sprite.gif')
+#draw sprite
 player = turtle.Turtle()
-player.shape("square")
-player.color("white")
+player.shape('sprite.gif')
 player.speed(0)
 player.penup()
 player.width = 20
@@ -47,6 +66,8 @@ class Block(object):
         self.width = width
         self.height = height 
         self.color = "green"
+    
+        
         
         
     def render(self, pen):
@@ -61,24 +82,30 @@ class Block(object):
     
     
 
-        
+#make the user jump        
 def jump():
     if player.state == "jumping_1":
         player.state = "jumping_2"
         player.dy = 5
+        os.system("afplay jump2.mp3&")
     if player.state == "ready":
         player.state = "jumping_1"
         player.dy = 5 
+        os.system("afplay jump1.mp3&")
     
-    
+#make the user move to left    
 def move_left():
     player.dx = -2
-        
+
+#make the user move to right        
 def move_right():
     player.dx = 2
-    
+
+#make the user stop    
 def stop_left_right():
     player.dx = 0
+
+
     
 def stop_down(ground,player_height):
     player.sety(ground_y+player.height/2) 
@@ -91,6 +118,11 @@ def is_collision(player, block):
     y_collision = math.fabs(player.ycor() - block.y) * 2 < (player.height + block.height)
     return (x_collision and y_collision)
 
+def teleport_sound():
+    os.system('afplay teleport.mp3&')
+    
+
+#make available for user to input
 wn.listen()
 wn.onkeypress(jump,"space") 
 wn.onkeypress(move_left,"Left")
@@ -98,8 +130,11 @@ wn.onkeypress(move_right,"Right")
 wn.onkeyrelease(stop_left_right, "Left")
 wn.onkeyrelease(stop_left_right, "Right")
 
+
+#place the boxes
 blocks = []
 blocks.append(Block(-600, 0, 2, 800))
+blocks.append(Block(600, 0, 2, 800))
 blocks.append(Block(0, -370, 1200,2))
 blocks.append(Block(-550, 300, 100, 2))
 blocks.append(Block(-500, 275, 2, 50))
@@ -124,14 +159,46 @@ blocks.append(Block(-420,-150,140,2))
 blocks.append(Block(-500,-250,250,2))
 blocks.append(Block(-170,-250,259,2))
 blocks.append(Block(-230,-140,2,470))
-blocks.append(Block(-140,-120,2,250))
+blocks.append(Block(-140,-120,2,245))
 blocks.append(Block(-40,5,200,2))
 blocks.append(Block(60,-35,2,680))
-blocks.append(Block(160,305,200,2))
+blocks.append(Block(159,308,200,2))
 blocks.append(Block(-255,-190,40,2))
+blocks.append(Block(-264,-100,60,2))
 blocks.append(Block(36,-310,50,2))
 blocks.append(Block(36,-200,50,2))
+blocks.append(Block(41,150,40,2))
+blocks.append(Block(-5,308,40,2))
 blocks.append(Block(-100,-170,80,2))
+blocks.append(Block(20,-110,80,2))
+blocks.append(Block(240,230,200,2))
+blocks.append(Block(340,230,2,155))
+blocks.append(Block(200,150,280,2))
+blocks.append(Block(415,308,150,2))
+blocks.append(Block(490,210,2,190))
+blocks.append(Block(575,230,50,2))
+blocks.append(Block(335,50,550,2))
+blocks.append(Block(460,115,60,2))
+blocks.append(Block(370,180,60,2))
+blocks.append(Block(270,-270,2,195))
+blocks.append(Block(210,-310,120,2))
+blocks.append(Block(122,-240,120,2))
+blocks.append(Block(290,-170,270,2))
+blocks.append(Block(540,-170,110,2))
+blocks.append(Block(475,-310,250,2))
+blocks.append(Block(440,-240,180,2))
+blocks.append(Block(350,-275,2,70))
+blocks.append(Block(280,-100,436,2))
+blocks.append(Block(450,-30,300,2))
+blocks.append(Block(200,-25,2,150))
+
+
+# ~ pen.goto(50,-20)
+# ~ pen.down()
+# ~ pen.shape('goal.gif')
+# ~ pen.up()
+
+
 
 #entrance to teleport
 entrances = []
@@ -139,7 +206,13 @@ entrances.append(Block(-595,-340, 3, 50))
 entrances.append(Block(-595,200, 3, 50))
 entrances.append(Block(-486,-120, 3, 50))
 entrances.append(Block(-135,-145,3,50))
- 
+entrances.append(Block(55,-85,3,50))
+entrances.append(Block(-185,-245,50,3))
+entrances.append(Block(335,179,3,50))
+entrances.append(Block(344,208,3,50))
+entrances.append(Block(355,-280,3,50))
+entrances.append(Block(595,-340,3,50))
+entrances.append(Block(595,-5,3,50))
 
 for entrance in entrances:
     entrance.color = "red"
@@ -148,20 +221,49 @@ for entrance in entrances:
 exits = []
 exits.append(Block(-280,388,50,3))
 exits.append(Block(-150,-255,50,3))
+exits.append(Block(-272,93,50,3))
+exits.append(Block(100,388,50,3))
+exits.append(Block(264,-345,3,50))
 for exit in exits:
     exit.color = "blue"
 
 gateways_list = [entrances,exits]
 
+
+# ~ player.goto(-500,400)
 player.goto(-500,400)
 
+# ~ def playerlife()
+    # ~ life.setposition(300,300)
+    # ~ life.write(life)
+
+player_life = 50
 while True:
-    pen.penup()
-    pen.goto(-430, 24)
-    pen.pendown()
-    pen.forward(120)
-    pen.right(1)
     
+    
+    
+    # ~ pen_x = [-425,550]
+    # ~ pen_y = [15,230]
+    # ~ pen_length = [100,50]
+    
+    # ~ for i in range(2):
+        # ~ pen.penup()
+        # ~ pen.goto(pen_x[i],pen_y[i])
+        # ~ pen.pendown()
+        # ~ pen.forward(pen_length[i])
+        # ~ pen.right(0.5)
+        
+        # ~ pen.shape("circle")
+        # ~ pen.color("red")
+        # ~ pen.stamp() 
+        # ~ pen.color("green")
+        # ~ # Check for collision with player
+        # ~ if player.distance(pen) < 20:
+            # ~ player_life = player_life - 1
+            # ~ print(player_life)
+        # ~ else:
+            # ~ print("")
+        
     
   
     
@@ -215,7 +317,7 @@ while True:
     pen.clear()
 
     
-    print(str(player.xcor()) + " " + str(player.ycor()))
+    # ~ print("x:"+str(player.xcor()) + " y: " + str(player.ycor()))
     
     
         
@@ -223,21 +325,63 @@ while True:
         player.goto(-280,400)
         player.dy = 0
         player.dx = 0
+        teleport_sound()
                     
     elif player.xcor() == -480  and (player.ycor()>=-144 and player.ycor()<=-95):
         player.goto(-147,-255)
         player.dy = 0
         player.dx = 0
+        teleport_sound()
         
-    elif player.xcor() == -129  and (player.ycor()>=-162 and player.ycor()<=-120):
-        player.goto(-147,-255)
+    
+        
+    elif player.xcor() == 49  and (player.ycor()>=-100 and player.ycor()<=-80):
+        player.goto(100,400)
         player.dy = 0
         player.dx = 0
+        teleport_sound()
         
-wn.onkeypress(up,"up")
-
-
-
+    elif (player.xcor() >= -220 and player.xcor() <= -151) and player.ycor() ==-240:
+        player.goto(-272,80)
+        player.dy = 0
+        player.dx = 0
+        teleport_sound()
+    
+    elif player.xcor() == -129  and (player.ycor()>=-162 and player.ycor()<=-120):
+        player.goto(-272,80)
+        player.dy = 0
+        player.dx = 0
+        os.system('afplay goal.mp3&')
+        
+    elif player.xcor() == 329  and (player.ycor()>=160 and player.ycor()<=200):
+        player.goto(100,400)
+        player.dy = 0
+        player.dx = 0
+        teleport_sound()
+        
+    elif player.xcor() == 589  and (player.ycor()>=-360  and player.ycor()<=-320):
+        player.goto(257,-350)
+        player.dy = 0
+        player.dx = 0
+        teleport_sound()
+            
+    elif player.xcor() == 351  and (player.ycor()>=190 and player.ycor()<=230):
+        player.goto(257,-350)
+        player.dy = 0
+        player.dx = 0
+        teleport_sound()
+    
+    elif player.xcor() == 361  and (player.ycor()>=-300  and player.ycor()<=-260):
+        player.goto(160,-30)
+        player.dy = 0
+        player.dx = 0
+        teleport_sound()
+        
+    elif player.xcor() == 589  and (player.ycor()>=-20  and player.ycor()<=20):
+        player.goto(-280,400)
+        player.dy = 0
+        player.dx = 0
+        teleport_sound()
+        
 
 wn.mainloop()
-
